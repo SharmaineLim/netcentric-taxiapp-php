41a114fe90262265c451f2245b0a74694ecef87a
TsugumiWinters <TsugumiWinters@github.com>
2013-11-02 18:28:30 +0800

    Added navigation to /home

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -3,5 +3,14 @@
 
 	<div id="body">
 		<p>Please move on forward to the other pages!</p>
+		<ul>Navigation:
+			<li><a href="index.php/account">Account</a></li>
+			<li><a href="index.php/taxi">Taxi</a></li>
+			<li><a href="index.php/report">Report</a></li>
+			<li><a href="index.php/comment">Comment</a></li>
+			<li><a href="index.php/subcategory">Subcategory</a></li>
+			<li><a href="index.php/category">Category</a></li>
+			<li><a href="index.php/level">Level</a></li>
+		</ul>
 	</div>
 </div>
