15f5a7f20bfea2a38ade54ca1439922a18ccfeb9
TsugumiWinters <TsugumiWinters@github.com>
2013-11-03 17:54:27 +0800

    Quick fix: Navigation in Home

--- taxi/application/views/home/index.php
+++ taxi/application/views/home/index.php
@@ -3,14 +3,21 @@
 
 	<p>Please move on forward to the other pages!</p>
 	
-	<ul>Navigation:
-		<li><a href="index.php/account">Account</a></li>
-		<li><a href="index.php/taxi">Taxi</a></li>
-		<li><a href="index.php/report">Report</a></li>
-		<li><a href="index.php/comment">Comment</a></li>
-		<li><a href="index.php/subcategory">Subcategory</a></li>
-		<li><a href="index.php/category">Category</a></li>
-		<li><a href="index.php/level">Level</a></li>
+	<ul>
+		<li>Navigation:</li>
+		<ul>
+			<li><a href="index.php/account">Account</a></li>
+			<li><a href="index.php/taxi">Taxi</a></li>
+			<li><a href="index.php/report">Report</a></li>
+			<li><a href="index.php/comment">Comment</a></li>
+			<li>References:</li>
+
+			<ul>
+				<li><a href="index.php/subcategory">Subcategory</a></li>
+				<li><a href="index.php/category">Category</a></li>
+				<li><a href="index.php/level">Level</a></li>
+			</ul>
+		</ul>
 	</ul>
 
 </div>
